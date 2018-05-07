a28c11f17f1e082ee0b2cf625418bce66e8a0029
MasazI <masaz.dream@gmail.com>
2018-05-08 00:04:59 +0900

    set image size

--- caminput.py
+++ caminput.py
@@ -3,6 +3,11 @@ print(cv2.__version__)
  
 cap = cv2.VideoCapture(0)
  
+# 画像サイズの指定
+cv.SetCaptureProperty(cap,cv.CV_CAP_PROP_FRAME_WIDTH,480)
+cv.SetCaptureProperty(cap,cv.CV_CAP_PROP_FRAME_HEIGHT,360)
+
+
 while( cap.isOpened() ):
  
     ret, frame = cap.read()
